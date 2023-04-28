d5f625d8da0e21ccfef5da8258beba9d178eb056
Yeansovanvathana <yean.sovanvathana19@kit.edu.kh>
2023-04-28 16:32:35 +0700

    Update responsive page

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,18 @@
 import React from "react";
 
 export const Header = () => {
-    return (
-        <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl-items-center">
-            <div className="flex flex-row items-center text-gray-300 cursor-poniter">
-                <p className="uppercase text-xl text-gray-600">Vathana</p>
-            </div>
-            <div className="hidden md:inline-flex space-x-16">
-                <a href="">Home</a>
-                <a href="">Home</a>
-                <a href="">Home</a>
-                <a href="">Home</a>
-            </div>
-            <button className="uppercase bg-gradient-to-r from-green-500 py-2 px-4 rounded text-sm hover:bg-green-500">
-                Contact
-            </button>
-            
-        </header>
-    );
+  return (
+    <header className="top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl-items-center">
+      <p className="uppercase text-xl text-gray-600">Vathana</p>
+      <div className="hidden uppercase md:inline-flex space-x-16 md:space-x-10 p-1">
+        <a href="">Home</a>
+        <a href="">Home</a>
+        <a href="">Home</a>
+        <a href="">Home</a>
+      </div>
+      <button className="uppercase bg-gradient-to-r from-green-500 py-2 px-4 rounded text-sm hover:bg-green-500">
+        Contact
+      </button>
+    </header>
+  );
 };
--- components/Page.js
+++ components/Page.js
@@ -3,29 +3,30 @@ import Image from "next/image";
 import Vathana from "@/public/Vathana.jpg";
 
 export const Page = () => {
-    return (
-        <div className="lg:inline-flex max-w-7xl mx-auto z-20 xl-items-center">
-            <div className=" mx-auto p-12 text-center">
-                <h2 className=" text-3xl py-2 font-medium">
-                    I'm <span className=" text-green-600">Yean</span>{" "}
-                    Sovanvathana
-                </h2>
-                <h3 className="text-2xl py-2">Developer and designer.</h3>
-                <p className="text-md py-5 leading-8 text-gray-500">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nunc felis neque, venenatis a volutpat et, ultrices.
-                </p>
-            </div>
-            <div className="bg-gray-300 w-48 h-72 mx-auto rounded-3xl relative">
-                <div className="w-48 h-72 mx-auto rounded-3xl absolute overflow-hidden right-5 -top-5">
-                    <Image
-                        src={Vathana}
-                        alt="Vathana"
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                </div>
-            </div>
+  return (
+    <div className="lg:flex md:flex justify-between max-w-7xl lg:mt-24 md:mt-24 px-10">
+      <div className="mx-auto p-10 text-center lg:text-start md:text-start">
+        <h2 className="text-3xl py-2 font-medium">
+          I'm <span className=" text-green-600">Yean</span> Sovanvathana
+        </h2>
+        <h3 className="text-xl py-2">Developer and designer.</h3>
+        <p className="text-md py-5 leading-8 text-gray-500 md:text-xl max-w-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis
+          neque, venenatis a volutpat et, ultrices.
+        </p>
+      </div>
+      <div className="flex justify-center items-center px-10 lg:px-0">
+        <div className="bg-gray-300 w-44 h-64 rounded-3xl relative lg:w-56 lg:h-80">
+          <div className="w-44 h-64 rounded-3xl absolute overflow-hidden right-5 -top-5 lg:w-56 lg:h-80">
+            <Image
+              src={Vathana}
+              alt="Vathana"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
