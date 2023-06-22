ffaaf9dadfa36c4ef62d37280065d06e2064510c
Yeansovanvathana <yean.sovanvathana19@kit.edu.kh>
2023-06-22 16:08:43 +0700

    Test page

--- pages/index.js
+++ pages/index.js
@@ -11,100 +11,100 @@ import { Project } from "@/components/Project";
 import { About } from "@/components/About";
 
 export default function Home() {
-  const [selectedPage, setSelectedPage] = useState("home");
-  const [isTopOfPage, setIsTopOfPage] = useState(true);
-  const isDesktop = useMediaQuery("(min-width: 1060px)");
+    const [selectedPage, setSelectedPage] = useState("home");
+    const [isTopOfPage, setIsTopOfPage] = useState(true);
+    const isDesktop = useMediaQuery("(min-width: 1060px)");
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setIsTopOfPage(true);
-        setSelectedPage("home");
-      }
-      if (window.scrollY !== 0) setIsTopOfPage(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY === 0) {
+                setIsTopOfPage(true);
+                setSelectedPage("home");
+            }
+            if (window.scrollY !== 0) setIsTopOfPage(false);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
-  return (
-    <div className="h-full w-screen overflow-scroll bg-primary-100 ">
-      <Head>
-        <title>Vathana Portfolio</title>
-        <link rel="icon" href="/Icons/v-logo.png" type="image/x-icon" />
-      </Head>
+    return (
+        <div className="bg-primary-100 ">
+            <Head>
+                <title>Vathana Portfolio</title>
+                <link rel="icon" href="/Icons/v-logo.png" type="image/x-icon" />
+            </Head>
 
-      <Navbar
-        isTopOfPage={isTopOfPage}
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
+            <Navbar
+                isTopOfPage={isTopOfPage}
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+            />
 
-      <div className="w-screen">
-        {isDesktop && (
-          <DotGroup
-            selectedPage={selectedPage}
-            setSelectedPage={setSelectedPage}
-          />
-        )}
+            <div className="">
+                {isDesktop && (
+                    <DotGroup
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                    />
+                )}
 
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("home")}
-        >
-          <Page />
-        </motion.div>
-      </div>
+                <motion.div
+                    margin="0 0 -200px 0"
+                    amount="all"
+                    onViewportEnter={() => setSelectedPage("home")}
+                >
+                    <Page />
+                </motion.div>
+            </div>
 
-      {/* <LineGradient /> */}
-      <div className="">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("skills")}
-        >
-          <Skillset />
-        </motion.div>
-      </div>
+            {/* <LineGradient /> */}
+            <div className="">
+                <motion.div
+                    margin="0 0 -200px 0"
+                    amount="all"
+                    onViewportEnter={() => setSelectedPage("skills")}
+                >
+                    <Skillset />
+                </motion.div>
+            </div>
 
-      <div className="">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("design")}
-        >
-          <Design />
-        </motion.div>
-      </div>
+            <div className="">
+                <motion.div
+                    margin="0 0 -200px 0"
+                    amount="all"
+                    onViewportEnter={() => setSelectedPage("design")}
+                >
+                    <Design />
+                </motion.div>
+            </div>
 
-      <div className="">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("experience")}
-        >
-          <Project />
-        </motion.div>
-      </div>
+            <div className="">
+                <motion.div
+                    margin="0 0 -200px 0"
+                    amount="all"
+                    onViewportEnter={() => setSelectedPage("experience")}
+                >
+                    <Project />
+                </motion.div>
+            </div>
 
-      <div className="">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("about")}
-        >
-          <About />
-        </motion.div>
-      </div>
+            <div className="">
+                <motion.div
+                    margin="0 0 -200px 0"
+                    amount="all"
+                    onViewportEnter={() => setSelectedPage("about")}
+                >
+                    <About />
+                </motion.div>
+            </div>
 
-      {/* About */}
+            {/* About */}
 
-      {/* Experience */}
+            {/* Experience */}
 
-      {/* Project */}
+            {/* Project */}
 
-      {/* Contact Me/ */}
-    </div>
-  );
+            {/* Contact Me/ */}
+        </div>
+    );
 }
